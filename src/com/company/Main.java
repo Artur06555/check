7fd33abb10727cc371e0627b16996d1e8eb6ab3e
Artur06555 <kurmanbekovvv0@gmail.com>
2021-02-28 09:23:12 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        String javaLeNd = "Num"+"person";
+        final int NUM = 0;
+
+
+        String word = "person";
+
+        System.out.println("javaLeNd");
+
+
+        if (NUM < 0) {
+            System.out.println("Вы сохранили отрицательное число");
+            if (NUM > 0) {
+                System.out.println("Вы сохранили положительное число");
+                if (NUM == 0) {
+                    System.out.println("Вы сохранили нуль");
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
